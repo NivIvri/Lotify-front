8f0108cabb44b3f63177fdb16b50dfc599581b0d
Niv Ivri <nivii123456@gmail.com>
2021-09-23 11:13:59 +0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+//import {HomePage} from './pages/home-page.jsx'
+//import {AboutUs} from './pages/about-us.jsx'
+//import {CarApp} from './pages/car-app.jsx'
+
+import { Home } from "./pages/Home";
+
+const routes = [
+    {
+        path:'/',
+        component: Home,
+    }
+    //{
+    //    path:'/car',
+    //    component: CarApp,
+    //},
+    //{
+    //    path:'/about',
+    //    component: AboutUs,
+    //}
+]
+
+export default routes;
