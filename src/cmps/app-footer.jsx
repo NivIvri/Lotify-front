c770a574fe1d485d5da4533ed339ca6220607e1c
Niv Ivri <nivii123456@gmail.com>
2021-09-23 21:43:58 +0300

    with working player

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -10,46 +10,122 @@ import play from '../assets/img/play.svg';
 import shuffle from '../assets/img/shuffle.svg';
 import ReactPlayer from 'react-player'
 import { connect } from 'react-redux'
+//function SeekBar({ played, duration, setPlayed }) {
+
+//    const secToHHMMSS = (secs) => {
+//        const pad = (num) => {
+//            return ("0" + num).slice(-2);
+//        }
+//        var minutes = Math.floor(secs / 60);
+//        secs = secs % 60;
+//        var hours = Math.floor(minutes / 60)
+//        minutes = minutes % 60;
+//        if (hours === 0) { return `${pad(minutes)}:${pad(secs)}`; }
+//        else if (hours !== 0) { return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`; }
+//    }
+//    var newPlayed = secToHHMMSS(Math.round(played));
+//    var newDuration = secToHHMMSS(duration);
+//    return (
+//        <div style={{ marginBottom: '10vh' }}>
+//            <input style={{ width: '90%', margin: '0 5% 0 5%', height: '2px', borderRadius: '10%' }} className="slider"
+//                type='range' min={0} value={Math.round(played)} max={duration}
+//                onChange={e => setPlayed(e.target.value)} />
+//            <span style={{ float: 'left', marginLeft: '5%', marginTop: '3%' }}>{newPlayed}</span>
+//            <span style={{ float: 'right', marginRight: '5%', marginTop: '3%' }}>{newDuration}</span>
+//        </div>
+//    )
+//}
+
 
 class _AppFooter extends Component {
     state = {
-        volum: 30,
-        isPlayedTrack: false
+        volume: 50,
+        isPlayedTrack: false,
+        played: 0,
+        loaded: 0,
+        duration: 0,
     }
 
+    componentDidUpdate(prevProps) {
+        if (this.props.currTrack !== prevProps.currTrack) {
+            this.setState({ isPlayedTrack: true, played: 0 })
 
-    
 
-    handleChange = (event, newValue) => {
-        this.setState({ volum: newValue });
-        console.log(this.state.volum);
+        }
+    }
+
+    handleChange = (event) => {
+        let newValue = event.target.value;
+        this.setState({ volume: newValue });
     };
     togglePlay = () => {
         this.setState({ isPlayedTrack: !this.state.isPlayedTrack })
     }
 
+
+    handleSeekMouseDown = e => {
+        this.setState({ seeking: true })
+    }
+
+    handleSeekChange = e => {
+        this.setState({ played: parseFloat(e.target.value) },
+            console.log(this.state.played, 'played')
+        )
+    }
+
+    handleSeekMouseUp = e => {
+        this.setState({ seeking: false })
+        this.player.seekTo(parseFloat(e.target.value))
+    }
+
+    handleProgress = state => {
+        console.log('onProgress', state)
+        // We only want to update time slider if we are not currently seeking
+        if (!this.state.seeking) {
+            this.setState(state)
+        }
+    }
+
+
+  
+
+    handleDuration = (duration) => {
+        console.log('onDuration', duration)
+        this.setState({ duration })
+    }
+
+    ref = player => {
+        console.log(player, 'player');
+        this.player = player
+    }
+
     render() {
-        const { isPlayedTrack } = this.state
-        const { currIdx, queue } = this.props
-        const track = queue[currIdx]
+        const { played, isPlayedTrack, volume } = this.state
+        console.log(played, 'played');
+        const track = this.props.currTrack
         return (
             <div className='playing-bar'>
                 {
                     track &&
                     <ReactPlayer
+                        ref={this.ref}
                         playing={isPlayedTrack}
                         url={`https://www.youtube.com/watch?v=${track.id}`}
                         onDuration={this.onDuration}
                         onProgress={this.onProgress}
                         width='0px'
                         heigth='0px'
+                        volume={volume / 100}
+                        onSeek={e => console.log('onSeek', e)}
+                        onProgress={this.handleProgress}
+
                     />
                 }
-                <div className="volum">
+                <div className="volume">
                     <Box sx={{ width: 200 }}>
                         <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
                             {/*<VolumeDown />*/}
-                            <Slider aria-label="Volume" value={this.state.value} onChange={this.handleChange} />
+                            <Slider aria-label="Volume" value={volume} onChange={this.handleChange} />
                             {/*<VolumeUp />*/}
                         </Stack>
                     </Box> </div>
@@ -69,8 +145,14 @@ class _AppFooter extends Component {
                         <img src={next} />
                     </div>
                     <div className='played-input'>
-                        <input type='range' />
-                    </div>
+                        <input
+                            type='range' min={0} max={0.999999} step='any'
+                            value={played}
+                            onMouseDown={this.handleSeekMouseDown}
+                            onChange={(ev) => this.handleSeekChange(ev)}
+                            onMouseUp={this.handleSeekMouseUp}
+                        /></div>
+
                 </div>
 
 
@@ -84,6 +166,10 @@ class _AppFooter extends Component {
                         <img className='track-img' src={track.imgUrl} />
                     }
                 </div>
+                <div>
+                    <progress max={1} value={played} />
+
+                </div>
             </div>
         )
     }
@@ -92,8 +178,7 @@ class _AppFooter extends Component {
 
 function mapStateToProps(state) {
     return {
-        currIdx: state.stationMoudle.currIdx,
-        queue: state.stationMoudle.queue
+        currTrack: state.stationMoudle.currTrack,
     }
 }
 const mapDispatchToProps = {
