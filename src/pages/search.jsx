91a25f4f38e72273b5263ff6caec3de57b0e62f6
Ori <orisharonn@gmail.com>
2021-09-28 09:52:54 +0300

    sdfsdff

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -58,7 +58,7 @@ export class SearchResult extends React.Component {
                         </div>
                         <span>
                             {trackResult[0].title}</span>
-                        
+
                     </div>
                     {trackResult.length > 1 &&
                         <TrackList songs={trackResult.slice(1, 5)} playTrack={(track, idx) => { this.props.setCurrTrack(track, idx) }} onAddToNextQueue={() => { console.log('hi'); }} />
@@ -102,7 +102,7 @@ export default class search extends React.Component {
     }, 700);
 
 
-    handleChange = async ({ target }) => {``
+    handleChange = async ({ target }) => {
         this.setState({ keySearch: target.value }, () => {
             if (this.state.keySearch === '' || this.state.keySearch === ' ') {
                 this.setState({ isOnSearch: false })
