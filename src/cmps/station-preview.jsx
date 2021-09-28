1020fafdc201d233766c47d719227be25d7cb2d6
Ori <orisharonn@gmail.com>
2021-09-28 13:16:40 +0300

    ss

--- src/cmps/station-preview.jsx
+++ src/cmps/station-preview.jsx
@@ -1,13 +1,27 @@
 import { Link } from 'react-router-dom'
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import { createMuiTheme, createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+// import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 export function StationPreview({ station }) {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#04d10b'
+            }
+        }
+    });
     return (
         <div className="station-preview">
             <Link className="img-card" to={`/station/${station._id}`}>
                 <div className="square-ratio img-container">
                     <img src={`${station.songs[0].imgUrl}`} />
-                    <PlayCircleFilledIcon></PlayCircleFilledIcon>
+                    <div className="play-icon">
+                        <ThemeProvider theme={theme}>
+                            <PlayCircleFilledIcon color="primary" fontSize="large"></PlayCircleFilledIcon>
 
+                        </ThemeProvider>
+                    </div>
                 </div>
                 <h3 className="station-name-header">{station.name}</h3>
                 <p className="station-desc">
