d0909064ed3d1e7886be038d0e7cf0c894f75990
Ori <orisharonn@gmail.com>
2021-10-09 14:24:50 +0300

    דגכ

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -10,32 +10,32 @@ import { Signin } from './pages/signin.jsx'
 import { Signup } from './pages/signup.jsx'
 
 export class RootCmp extends React.Component {
-    componentDidMount() {
+    // componentDidMount() {
 
-        return new Promise(resolve => {
+    //     return new Promise(resolve => {
 
-            window.fbAsyncInit = function () {
-                window.FB.init({
-                    appId: '992060094973798',
-                    cookie: true,
-                    xfbml: true,
-                    version: 'v12.0'
-                });
+    //         window.fbAsyncInit = function () {
+    //             window.FB.init({
+    //                 appId: '992060094973798',
+    //                 cookie: true,
+    //                 xfbml: true,
+    //                 version: 'v12.0'
+    //             });
 
-                window.FB.AppEvents.logPageView();
+    //             window.FB.AppEvents.logPageView();
 
-            };
+    //         };
 
-            (function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) { return; }
-                js = d.createElement(s); js.id = id;
-                js.src = "https://connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+    //         (function (d, s, id) {
+    //             var js, fjs = d.getElementsByTagName(s)[0];
+    //             if (d.getElementById(id)) { return; }
+    //             js = d.createElement(s); js.id = id;
+    //             js.src = "https://connect.facebook.net/en_US/sdk.js";
+    //             fjs.parentNode.insertBefore(js, fjs);
+    //         }(document, 'script', 'facebook-jssdk'));
 
-        })
-    }
+    //     })
+    // }
 
 
     render() {
