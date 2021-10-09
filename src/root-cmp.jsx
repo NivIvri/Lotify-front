ee5110a724b0bbf166c65cd58daac2eea486c71b
Ori <orisharonn@gmail.com>
2021-10-09 13:55:54 +0300

    דגכדגכג

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -4,13 +4,36 @@ import { MainNav } from './cmps/main-nav.jsx'
 import routes from './routes.js'
 import { AppFooter } from './cmps/app-footer.jsx'
 import { CreateStation } from './cmps/create-playlist.jsx'
-import { UserProfile} from './cmps/profile'
+import { UserProfile } from './cmps/profile'
 import { UserMsg } from './cmps/user-msg.jsx'
 import { Signin } from './pages/signin.jsx'
 import { Signup } from './pages/signup.jsx'
 
 export class RootCmp extends React.Component {
+    componentDidMount() {
 
+
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: '992060094973798',
+                cookie: true,
+                xfbml: true,
+                version: '12.0'
+            });
+
+            window.FB.AppEvents.logPageView();
+
+        };
+
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+    }
     render() {
         return (
             <>
