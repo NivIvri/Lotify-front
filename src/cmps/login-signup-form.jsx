b00f58065f1443b04ea1c74fb7a18b49388a0f34
Ori <orisharonn@gmail.com>
2021-10-09 10:41:15 +0300

    sadsdas

--- src/cmps/login-signup-form.jsx
+++ src/cmps/login-signup-form.jsx
@@ -4,16 +4,17 @@ import logo from '../assets/img/gramophone-svgrepo-com.svg'
 
 export class LoginSignupForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       // credentials: {
       username: '',
       password: '',
-      fullname: ''
+      fullname: '',
+      isLogin: this.props.isLogin
       // facebookUserId: null,
       // img: null
       // }
-    }
+    };
   }
 
   responseFacebook = async response => {
@@ -48,44 +49,64 @@ export class LoginSignupForm extends Component {
   resetCredentials = () => {
     this.setState({ username: '', fullname: '', password: '', facebookUserId: null, img: null })
   }
+
   handleClose = (e) => {
     this.resetCredentials()
     this.props.closeForm()
   }
+
+  handleSignIn = () => {
+    // console.log('signing');
+    this.setState(ps => ({ ...ps, isLogin: false }))
+  }
+
+  handleLogin = () => {
+    this.setState(ps => ({ ...ps, isLogin: true }))
+  }
+
   render() {
-    const { username, fullname, password } = this.state
+    const { username, fullname, password, isLogin } = this.state
     return (
-      <section className="form-wrapper">
-        <span className="close-form" onClick={this.handleClose}>X</span>
-        <div className="form-logo">
-          <img src={logo} alt="" />
-        </div>
-        <h2 className="form-header">{this.props.isLogin ? 'Sign in to continue' : 'Signup to continue'}</h2>
-        <form action="" className="login-signup-form flex column" onSubmit={this.handleSubmit}>
-          <input type="text" name="username" placeholder="Enter your username" value={username}
-            onChange={this.handleChange} />
+      <>
+        <div className="screen" onClick={this.handleClose}></div>
+        <section className="form-wrapper">
+          <span className="close-form" onClick={this.handleClose}>X</span>
+          <div className="form-logo flex column align-center">
+            <img src={logo} alt="" />
+            <div className="brand-name"><span>Lotify</span><span>.</span></div>
 
-          {!this.props.isLogin && <input type="text" name="fullname" placeholder="Enter your FullName"
-            value={fullname} onChange={this.handleChange} />}
+          </div>
+          <h2 className="form-header">{isLogin ? 'Sign in to continue' : 'Signup to continue'}</h2>
+          <form action="" className="login-signup-form flex column align-center" onSubmit={this.handleSubmit}>
+            <input type="text" name="username" placeholder="Enter your username" value={username}
+              onChange={this.handleChange} />
 
-          <input type="password" name="password" placeholder="Enter your password" value={password}
-            onChange={this.handleChange} />
+            {!isLogin && <input type="text" name="fullname" placeholder="Enter your FullName"
+              value={fullname} onChange={this.handleChange} />}
 
-          <button type="submit" className="login-signup-btn">{this.props.isLogin ? 'Login' : 'Signup'}</button>
-          <button className="facebook-login-btn-wrapper" type="button">
-            {this.props.isLogin ? 'Or Login with Facebook' : 'Or Signup with Facebook'}
+            <input type="password" name="password" placeholder="Enter your password" value={password}
+              onChange={this.handleChange} />
+            {/* <div className="submit-btns flex"> */}
+            <button type="submit" className="login-signup-btn">{isLogin ? 'Login' : 'Signup'}</button>
+            <button className="facebook-login-btn-wrapper" type="button">
+              {isLogin ? 'Login with Facebook' : 'Signup with Facebook'}
 
-            <FacebookLogin
-              className="facebook-login-btn"
-              appId="992060094973798"
-              autoLoad={false}
-              fields="name,email,picture"
-              onClick={this.facebookComponentClicked}
-              callback={this.responseFacebook} />
-          </button>
-        </form>
-      </section>
-
+              <FacebookLogin
+                className="facebook-login-btn"
+                appId="992060094973798"
+                autoLoad={false}
+                fields="name,email,picture"
+                onClick={this.facebookComponentClicked}
+                callback={this.responseFacebook} />
+            </button>
+            {isLogin && <div className="to-sign-in flex"><span>Dont have an acount?</span>
+              <span onClick={this.handleSignIn}>Sign here</span>
+            </div>}
+            {!isLogin && <span className="back-to-login" onClick={this.handleLogin}>Back to Login</span>}
+            {/* </div> */}
+          </form>
+        </section>
+      </>
     )
   }
 }
