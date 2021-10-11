65d6ec0bea66e0c2a45058eb9a43c40b82bfc430
Niv Ivri <nivii123456@gmail.com>
2021-10-11 12:09:23 +0300

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ class _Home extends Component {
 
     }
     async componentDidMount() {
-        console.log("🚀 ~ file: Home.jsx ~ line 50 ~ _Home ~ componentDidMount ~ componentDidMount")
+    console.log("🚀 ~ file: Home.jsx ~ line 50 ~ _Home ~ componentDidMount ~ componentDidMount")
         await this.props.loadUser();
         await this.props.loadStations();
         await this.getLikedStation()
